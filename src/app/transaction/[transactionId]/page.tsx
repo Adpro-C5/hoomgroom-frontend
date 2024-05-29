9d443d280d074cef741ce3b2dc89cc5e6f1182a2
evelynphs <evelyn.p.h.silalahi@gmail.com>
2024-05-29 19:55:21 +0700

    Make pages for transaction list and transaction detail

--- src/app/transaction/[transactionId]/page.tsx
+++ src/app/transaction/[transactionId]/page.tsx
@@ -1,18 +1,61 @@
-async function getTransaction(transactionId: string){
-    const res = await fetch(`http://localhost:8080/transaction/${transactionId}`);
-    return await res.json();
-}
+'use client'
 
-export default async function TransactionPage({params}: any){
-    const transaction = await getTransaction(params.transactionId);
-    console.log(transaction);
+import React, {useEffect, useState} from "react";
+
+export default function TransactionPage({params}:any){
+    let [transaction, setTransaction] = useState({});
+    let [error, setError] = useState(null);
+    let [productName, setProductName] = useState("");
+
+    useEffect(() => {
+        fetch(`http://34.101.211.204/transaction/${params.transactionId}`, {
+            headers:{
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem('token')
+            }
+        })
+            .then(response => {
+                if (!response.ok) {
+                    if(response.status == 403){
+                        throw new Error(`${response.body}`);
+                    }
+                    else{
+                        throw new Error(`${response.status}`);
+                    }
+                }
+                return response.json()
+            })
+            .then(data => {
+                setTransaction(data)
+
+                fetch(`http://34.87.141.138/product/${data.productId}`, {
+                    headers:{
+                        'Content-Type': 'application/json',
+                    }
+                })
+                    .then(response => response.json())
+                    .then(data => {
+                        setProductName(data.productName)
+                    })
+            })
+            .catch(error => {
+                setError(error);
+            })
+    }, [params.transactionId]);
+
+    if(error){
+        return <div>{error.message}</div>;
+    }
 
     return(
-        <div>
-            <h1>Transaction</h1>
-            <div>
-                <p>Transaction ID: {transaction.transactionId}</p>
-                <p>Product ID: {transaction.productId}</p>
+        <div className="flex flex-col items-center">
+            <h1 className="font-bold text-2xl text-blue-900 py-5">Transaction Detail</h1>
+            <h1>ID : {transaction.transactionId}</h1>
+            <div className="space-y-5 my-6 px-5 py-5 bg-blue-900 text-white rounded-md text-sm font-medium">
+                <p>Product: {productName} - {transaction.productId}</p>
+                <p>Product amount: {transaction.productAmount}</p>
+                <p>Promo code used in this
+                    transaction: {transaction.promoCode ? transaction.promoCode : "No promo code"}</p>
                 <p>Total Price: {transaction.totalPrice}</p>
             </div>
         </div>
--- src/app/transaction/list/page.tsx
+++ src/app/transaction/list/page.tsx
@@ -1,36 +1,84 @@
+'use client'
 import Link from "next/link";
+import React, {useEffect, useState} from 'react'
 
-async function getAllTransaction(){
-    const res = await fetch("http://localhost:8080/transaction/all");
-    return await res.json();
-}
 
-export default async function TransactionListPage(){
-    const transactions = await getAllTransaction();
-    console.log(transactions);
+export default function TransactionListPage(){
+
+    let[userid, setUserId] = useState(0)
+    let [transactions, setTransactions] = useState([])
+    let [error, setError] = useState(null);
+
+    useEffect(() => {
+        console.log("Token: " + localStorage.getItem('token'))
+        fetch("http://34.143.253.15/profile", {
+            headers:{
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem('token')
+            }
+        })
+            .then(response => {
+                if (!response.ok) {
+                    if(response.status == 401 || response.status == 400){
+                        throw new Error(`Login untuk melihat daftar transaksi Anda`);
+                    }
+                    else{
+                        throw new Error(`${response.status}`);
+                    }
+                }
+                return response.json()
+            })
+            .then(data => {
+                console.log('Profile: ' + JSON.stringify(data))
+                setUserId(data.id)
+
+                // fetch the transactions
+                fetch(`http://34.101.211.204/transaction/user/${data.id}`, {
+                    headers:{
+                        'Content-Type': 'application.json',
+                        'Authorization': 'Bearer ' + localStorage.getItem('token')
+                    }
+                })
+                    .then(response => response.json())
+                    .then(data => {
+                        console.log('Transaction: ' + JSON.stringify(data))
+                        setTransactions(data)
+                    })
+            })
+            .catch(error => {
+                setError(error);
+            })
+        // const token = localStorage.getItem('token')
+        // console.log(token)
+    }, []);
+
+    if(error){
+        return <div>{error.message}</div>;
+    }
+
+    if(transactions.length === 0){
+        return <div>
+            <h1>Anda belum membuat transaksi apapun</h1>
+        </div>;
+    }
 
     return(
-        <div>
-            <h1>Transaction</h1>
-            <div>
-                {transactions?.map((transaction: any) => {
-                    return <Transaction key={transaction.transactionId} transaction={transaction}/>;
-                })}
+        <div className="flex flex-col items-center">
+            <h1 className="font-bold text-4xl text-blue-900 py-5">Your Transactions</h1>
+            {/*<p>User userId : {userid}</p>*/}
+            <div className = "space-y-5 w-2/5">
+                {transactions.map((transaction) =>
+                    <div key={transaction.transactionId} className="bg-amber-500 w-full text-white rounded-md text-sm font-medium">
+                        <Link href={`/transaction/${transaction.transactionId}`}>
+                            <div className="px-3 py-3 space-y-2">
+                                <p>Transaction ID: {transaction.transactionId}</p>
+                                <p>Product ID: {transaction.productId}</p>
+                                <p>Total Price: {transaction.totalPrice}</p>
+                            </div>
+                        </Link>
+                    </div>
+                )}
             </div>
         </div>
-    );
-}
-
-function Transaction({transaction}: any){
-    const {transactionId, userId, productId, totalPrice} = transaction || {};
-
-    return(
-      <Link href = {`/transaction/${transactionId}`}>
-        <div>
-            <h2>Product ID: {productId}</h2>
-            <h5>User ID: {userId}</h5>
-            <p>Total Price: {totalPrice}</p>
-        </div>
-      </Link>
     );
 }
